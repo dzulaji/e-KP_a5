4745535dba9b3217ce42aa0c3723c8c50127e790
dzulaji <89774997+dzulaji@users.noreply.github.com>
2022-09-10 08:10:47 +0700

    Activity SplashScreen+Login

--- app/src/main/java/id/ac/unand/e_kp/SplashScreen.java
+++ app/src/main/java/id/ac/unand/e_kp/SplashScreen.java
@@ -2,7 +2,10 @@ package id.ac.unand.e_kp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
+
+        // inisialisasi view
+        View decorView = getWindow().getDecorView();
+        // Hide the status bar.
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
+        // Hide ActionBar
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+        // timer
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this, Login.class));
+                finish();
+            }
+        }, 3000);
     }
 }
